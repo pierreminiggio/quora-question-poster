831c3ab1c4f8990d3dcc82c140931b9d7b9b831b
Pierre MINIGGIO <pierre@miniggiodev.fr>
2021-01-21 21:03:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# facebquora-question-poster
+
+Prérequis :
+Ceux de Puppeteer : https://github.com/puppeteer/puppeteer
+
+Installation :
+```
+npm install pierreminiggio/facebquora-question-poster
+```
+
+Utilisation : 
+```javascript
+import post from '@pierreminiggio/facebquora-question-poster'
+
+await post('Facebook login or email', 'Facebook password', 'Facebook page name', 'Post content')
+```
---
+++ index.js
@@ -0,0 +1,56 @@
+import puppeteer from 'puppeteer'
+
+/**
+ * @typedef {Object} QuoraQuestionPosterConfig
+ * @property {boolean} show default false
+ * 
+ * @param {string} login
+ * @param {string} password
+ * @param {string} content
+ * @param {QuoraQuestionnfig} config 
+ * 
+ * @returns {Promise<string>}
+ */
+export default function (login, password, content, config = {}) {
+
+    return new Promise(async (resolve, reject) => {
+        
+        setDefaultConfig(config, 'show', false)
+
+        let browser
+        try {
+            browser = await puppeteer.launch({
+                headless: ! config.show,
+                args: [
+                    '--disable-notifications',
+                    '--no-sandbox'
+                ]
+            })
+        } catch (e) {
+            reject(e)
+            return
+        }
+        
+        try {
+            const page = await browser.newPage()
+
+            resolve('quora question id')
+        } catch (e) {
+            await browser.close()
+            reject(e)
+        }
+    })
+}
+
+/**
+ * @param {QuoraQuestionnfig} config 
+ * @param {string} configKey 
+ * @param {*} defaultValue
+ * 
+ * @returns {void}
+ */
+function setDefaultConfig(config, configKey, defaultValue) {
+    if (! (configKey in config)) {
+        config[configKey] = defaultValue
+    }
+}
